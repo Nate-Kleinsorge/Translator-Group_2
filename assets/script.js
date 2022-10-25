64b9d9b0a8ab5ad852e37005f3c0cad4aa57ca2c
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2022-10-24 21:05:16 -0400

    added word and definition

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 var key = {'X-RapidAPI-Key': '287257e633mshd41c1d94590619dp1e2d05jsnd9dc792f435b'};
 document.getElementById("translateBtn").addEventListener("click", function() {
     var word = document.getElementById("wordSearch").value;
-    console.log(word)
+    url = "https://wordsapiv1.p.rapidapi.com/words/" + word + "/definition";
+    fetchUrl(url);
     return word;
 })
 
@@ -10,18 +11,22 @@ var wordDef = document.getElementById("wordDef");
 var url = "https://wordsapiv1.p.rapidapi.com/words/" + word + "/definition";
 
 //fetch definitions
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '287257e633mshd41c1d94590619dp1e2d05jsnd9dc792f435b',
-		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-	}
-};
 
-fetch(url, options)
-	.then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data);
-    });
+function fetchUrl(url) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '287257e633mshd41c1d94590619dp1e2d05jsnd9dc792f435b',
+            'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+        }
+    };
+    
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            wordDis.textContent = data.word;
+            wordDef.textContent = data.definition;
+        });
+};
 
