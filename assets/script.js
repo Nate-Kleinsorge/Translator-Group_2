642c0e38aed7b34cd68ffd2fb492c516614acb49
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2022-10-27 19:35:26 -0400

    Created translate funtion

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 const lastFive = [localStorage.getItem("lastFive")];
 document.getElementById("translateBtn").addEventListener("click", function() {
+    const language = document.getElementById("langSelector").value;
     var word = document.getElementById("wordSearch").value;
     url = "https://wordsapiv1.p.rapidapi.com/words/" + word + "/definition";
     fetchUrl(url);
+    translate(word, language);
     word = word.trim();
     return word;
 });
@@ -53,22 +55,23 @@ function fetchUrl(url) {
 };
 
 //fetch translation and print
-
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Key': "287257e633mshd41c1d94590619dp1e2d05jsnd9dc792f435b",
-		'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
-	},
-	body: '[{"Text":"I would really like to drive your car around the block a few times."}]'
+function translate(input, language) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': "287257e633mshd41c1d94590619dp1e2d05jsnd9dc792f435b",
+            'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+        },
+        body: `[{"Text":"${input}"}]`
+    };
+    
+    fetch (`https://microsoft-translator-text.p.rapidapi.com/translate?to=${language}&api-version=3.0&profanityAction=NoAction&textType=plain`, options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
 };
 
-fetch ("https://microsoft-translator-text.p.rapidapi.com/translate?to=es&to=it&api-version=3.0&profanityAction=NoAction&textType=plain", options)
-	.then(function (response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
-        
